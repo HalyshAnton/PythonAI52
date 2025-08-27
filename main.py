c5331cc8ab8ae5f3cb041c15f8b61b4ba10ed259
HalyshAnton <kyhar007@gmail.com>
2025-08-27 19:12:54 +0300

    add lesson3

--- main.py
+++ main.py
@@ -1,121 +1,139 @@
-# Завдання 1
-# Створіть клас Проект з атрибутами:
-#  назва
-#  виділений кошторис
-#  загальні витрати
-#  чи завершений(за замовчуванням False)
-#  час виконання(за замовчуванням 0 місяців)
-#  список необхідних задач
-# Додайте методи:
-#  вивід інформації: назва, час виконання, необхідні
-# задачі
-#  добавити нову задачу
-#  розбити задачу на під-задачі: передається назва задачі
-# та список під-задач
-#  виконати задачу, передається назва, час та ціна
-# виконання
-#  поповнення кошторису
+# class Project:
+#     def __init__(self, name: str, budget: int, tasks: list):
+#         self.name = name
+#         self.budget = budget
+#         self.tasks = tasks
+#
+#         self.expenses = 0
+#         self.is_finished = False
+#         self.time = 0
+#
+#     def show_info(self):
+#         """
+#         Показує інформацію про проект
+#         """
+#
+#         print()
+#         print(f"Інформація по проекту {self.name}")
+#         print(f'\t Бюджет -- {self.budget}грн')
+#         print(f"\t Використано -- {self.expenses}/{self.budget}")
+#         print(f"\t Час виконання -- {self.time} місяців")
+#
+#         if self.is_finished:
+#             print(f'\t Статус -- Завершений')
+#         else:
+#             print(f'\t Статус -- Незавершений')
+#
+#         print('\t Список задач:')
+#         for task in self.tasks:
+#             print(f"\t\t {task}")
+#
+#     def add_task(self, new_task):
+#         self.tasks.append(new_task)
+#         print(f"Додано нове завдання {new_task}")
+#
+#     def create_subtasks(self, old_task, subtasks):
+#         # чи є стара задачі в списку
+#         if old_task not in self.tasks:
+#             print(f"Такої задачі немає в списку")
+#             return
+#
+#         # old_task є в списку
+#         self.tasks.remove(old_task)
+#         self.tasks.extend(subtasks)  # добавити всі елементи з subtasks в список self.tasks
+#
+#         # # спосіб де багато відступів
+#         # if old_task in self.tasks:
+#         #     # old_task є в списку
+#         #     self.tasks.remove(old_task)
+#         #     self.tasks.extend(subtasks)  # добавити всі елементи з subtasks в список self.tasks
+#         # else:
+#         #     print(f"Такої задачі немає в списку")
+#
+#     def do_task(self, task, price, time):
+#         if task not in self.tasks:
+#             print(f"Такої задачі немає в списку")
+#             return
+#
+#         if price > (self.budget - self.expenses):
+#             print('Не вистачає коштів')
+#             return
+#
+#         # все добре, робимо задачу
+#         self.tasks.remove(task)
+#         self.expenses += price
+#         self.time += time
+#
+#         self.is_finished = len(self.tasks) == 0
+#
+#     def deposit(self, price):
+#         self.budget += price
 
 
-# опис класу
-# (шаблон який описує всі проекти)
-class Project:
-    def __init__(self, name: str, budget: int, tasks: list):
+# Інкапсуаляція
+
+# nums = [1, 2, 3, 4]
+# nums.append(5)
+
+
+# Поліморфізм
+
+
+class Cat:
+    def __init__(self, name, age, color):
         self.name = name
-        self.budget = budget
-        self.tasks = tasks
+        self.age = age
+        self.color = color
 
-        self.expenses = 0
-        self.is_finished = False
-        self.time = 0
+    def make_sound(self):
+        print('Мяу')
 
-    def show_info(self):
-        print()
-        print(f"Інформація по проекту {self.name}")
-        print(f'\t Бюджет -- {self.budget}грн')
-        print(f"\t Використано -- {self.expenses}/{self.budget}")
-        print(f"\t Час виконання -- {self.time} місяців")
+    def grow(self):
+        self.age += 1
 
-        if self.is_finished:
-            print(f'\t Статус -- Завершений')
-        else:
-            print(f'\t Статус -- Незавершений')
-
-        print('\t Список задач:')
-        for task in self.tasks:
-            print(f"\t\t {task}")
-
-    def add_task(self, new_task):
-        self.tasks.append(new_task)
-        print(f"Додано нове завдання {new_task}")
-
-    def create_subtasks(self, old_task, subtasks):
-        # чи є стара задачі в списку
-        if old_task not in self.tasks:
-            print(f"Такої задачі немає в списку")
-            return
-
-        # old_task є в списку
-        self.tasks.remove(old_task)
-        self.tasks.extend(subtasks)  # добавити всі елементи з subtasks в список self.tasks
-
-        # # спосіб де багато відступів
-        # if old_task in self.tasks:
-        #     # old_task є в списку
-        #     self.tasks.remove(old_task)
-        #     self.tasks.extend(subtasks)  # добавити всі елементи з subtasks в список self.tasks
-        # else:
-        #     print(f"Такої задачі немає в списку")
-
-    def do_task(self, task, price, time):
-        if task not in self.tasks:
-            print(f"Такої задачі немає в списку")
-            return
-
-        if price > (self.budget - self.expenses):
-            print('Не вистачає коштів')
-            return
-
-        # все добре, робимо задачу
-        self.tasks.remove(task)
-        self.expenses += price
-        self.time += time
-
-        self.is_finished = len(self.tasks) == 0
-
-    def deposit(self, price):
-        self.budget += price
+    def catch_mouse(self):
+        pass
 
 
+class Dog:
+    def __init__(self, name, age, speed):
+        self.name = name
+        self.age = age
+        self.speed = speed
+
+    def make_sound(self):
+        print('Гав')
+
+    def grow(self):
+        self.age += 1
+        self.speed -= 2
 
 
-# створення конкретного проекту
-project1 = Project('Making Film', 10000, ['write plot', 'find actors'])
-project2 = Project('Create Site', 5000, ['create name', 'create database'])
+class Hamster:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-# print(project1.tasks)
-# print(project1.is_finished)
+    def make_sound(self):
+        print('Пі-пі')
 
-project1.show_info()
-
-project1.add_task("find location")
-project1.show_info()
-print(project1.tasks)
+    def grow(self):
+        self.age += 1
 
 
-project1.create_subtasks('talk with producer', [])
+cat = Cat('Murchick', 3, 'black')
+dog = Dog('Barsik', 2, 10)
+hamster = Hamster('Benny', 4)
 
-project1.create_subtasks("find actors",
-                         ['talk with actor1', 'talk with actor2'])
-project1.show_info()
+pets = [cat, dog, hamster]
 
-project1.do_task('talk with actor1', 1000, 0.5)
-project1.show_info()
+# кожна тварина видає звуки
+for pet in pets:
+    pet.make_sound()
 
-project1.do_task('write plot', 20000, 3.5)
 
-project1.deposit(50000)
-project1.do_task('write plot', 20000, 3.5)
-project1.show_info()
+# model = NeuralNetwork()
+#
+# model.train(data)
 
 
