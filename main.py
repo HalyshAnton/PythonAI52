1d70cfcce14c7a065440e145aeb8196c0150f199
HalyshAnton <kyhar007@gmail.com>
2025-10-29 19:52:24 +0200

    add lesson4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import redis
 import base64
+import json
 
 
 class SocialApp:
@@ -50,6 +51,54 @@ class SocialApp:
         else:
             print("Пароль невірний")
 
+    # ■ добавити інформацію про користувача;
+    def _get_data_key(self):
+        return f"data:{self.current_user}"
+
+    def add_user_data(self, user_data):
+        """
+        user_data повинна міститти дані які кодуються в json
+
+        :param user_data:
+        :return:
+        """
+        # user_data = {
+        #     'name': 'Anton',
+        #     'age': 23,
+        #     'articles': ['article1', 'article1', 'article1']
+        # }
+        if self.current_user is None:
+            print("Ввійдіть у систему")
+            return
+
+        data_key = self._get_data_key()
+
+        for key, value in user_data.items():
+            #переводимо дані у json(str рядок)
+            value = json.dumps(value)
+
+            self.server.hset(data_key, key, value)
+
+    # ■ добавити друзів;
+    def _get_friend_key(self, username):
+        return f"friends:{username}"
+
+    def add_friend(self, friend_name):
+        if self.current_user is None:
+            print("Ввійдіть у систему")
+            return
+
+        #перевірка чи friend_name зареєстрований
+        friend_key = self._get_password_key(friend_name)
+        if not self.server.exists(friend_key):
+            print(f"Такої людини не існує {friend_name}")
+            return
+
+        key1 = self._get_friend_key(self.current_user)
+        self.server.sadd(key1, friend_name)
+
+        key2 = self._get_friend_key(friend_name)
+        self.server.sadd(key2, self.current_user)
 
     # ■ видалити користувача;
     # ■ редагувати інформацію про користувача;
@@ -60,7 +109,18 @@ class SocialApp:
 
 app = SocialApp()
 app.add_user('Anton', 'qwerty123')
+app.add_user('Jhon', 'kygkhgkhghs35445')
 
 app.login('Jhon', 'qwerty123')
 app.login('Anton', 'qwerty1234')
 app.login('Anton', 'qwerty123')
+
+# user_data = {
+#             'name': 'Anton',
+#             'age': 23,
+#             'articles': ['article1', 'article1', 'article1']
+#         }
+# app.add_user_data(user_data)
+
+app.add_friend("Maria")
+app.add_friend("Jhon")
